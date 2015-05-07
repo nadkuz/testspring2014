e25b41352c4ec26d82e5620c28df14f5904c75e2
Nadia Kuznetsova <nadkuz@users.noreply.github.com>
2015-05-06 20:56:47 -0700

    Update htmlformat.sh

--- htmlformat.sh
+++ htmlformat.sh
@@ -1,44 +1,49 @@
-#!/bin/bash +cx
-# 
-
+#!/bin/bash 
+#
 
 if [[ -z "$1" || -z "$2" || ! -f "$1" ]];
   then
-    
     echo "htmlformat.sh [file to format] [file to write output]";
-    exit 1
+    return 1
 fi
 
 echo "Start processing.."
-
+rm $2
 echo "Add new lines"
 n=0
-while read -r line; do 
-#   echo "Got line $line"
-   IFS=$'\n';line2=$(echo "$line" | sed -e 's/\(.*\)\<\(.*\)\>\(.*\)/\1\\\n<\2>\\\n\3/g'|sed -e  's/\\\n\\\n/\\\n/g')
-#   echo "   add indents"
+indent=""
+curline=""
 cr='
 '
-indent=""
-for line3 in ${line2//\\\n/$cr}
+
+while read -r line; do
+   IFS=$'\n';line2=$(echo "$line" | sed -e 's/<\([\/a-zA-Z]*\)>/\n\<\1\>\n/g' |sed -e  's/\n\n/\n/g')
+for line3 in ${line2//\\\n/$cr};
 do
-#        echo "$(( ++c )) |$line3|"
-#       indent=""
+#       echo "$(( ++c )) |$line3|$curline|"
        if [[ "$line3" =~ ^"<".*">"$ ]]; then
-#          echo "got tag"
           if [[ "$line3" == \<\/* ]]; then
             indent=$(printf "%$(((n-1)*3))s")
              (( --n ))
+             if [[ -z "$curline" ]]; then
+                curline="$indent$line3"
+             else
+                curline="$curline$line3"
+             fi
+             echo "$curline" >> $2
+             curline=""
           else
+             if [[ ! -z "$curline" ]]; then
+                echo "$curline" >> $2
+             fi
              (( ++n ))
              indent=$(printf "%$(((n-1)*3))s")
+             curline="$indent$line3"
           fi
-           
+       else
+          curline="$curline$line3"
        fi
-       echo "$indent$line3" >> $2
-           
-       
-	
+
     done
 done  < $1
 
